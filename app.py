a3ea7963cf85b56d3472d14918898a14fd8327bf
david-morales <dmorales@dmorales.home>
2023-04-17 09:44:51 +0200

    Add buildconfif

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def welcome():
     collection = db['names']
     name = collection.find_one()['name']
     client.close()
-    return f"Welcome dear {name}"
+    rc = '\n'
+    return f"Welcome to a cloud native hybrid application, {name}! {rc}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
